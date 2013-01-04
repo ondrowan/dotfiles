5b8abb5952c4c233b0b62cd16b644e108fde90be
ondrowan <ondrowan@gmail.com>
2013-01-04 18:55:46 +0100

    Added echoes to install script

--- install.sh
+++ install.sh
@@ -6,10 +6,12 @@ SITES=~/Sites
 DOTFILES=~/.dotfiles
 
 if [ ! -d $CODE ]; then
+    echo "Creating $CODE directory..."
     mkdir $CODE
 fi
 
 if [ ! -d $SITES ]; then
+    echo "Creating $SITES directory..."
     mkdir $SITES
 fi
 
@@ -17,26 +19,37 @@ fi
 sudo apt-get install git vim zsh pidgin ack curl mc python-setuptools apache2 mysql-server postgresql php5 php5-cli php-pear php5-curl php5-gd php5-imagick php5-intl php5-mongo php5-mysqlnd php5-pgsql php5-sqlite php5-suhosin php5-xdebug
 
 # pip & virtualenv
-sudo easy_install pip
+
+if command -v pip > /dev/null 2>&1; then
+    echo "Installing pip..."
+    sudo easy_install pip
+fi
+
 sudo pip install virtualenv virtualenvwrapper
 
 # oh-my-zsh
 if [ -d ~/.oh-my-zsh ]; then
+    echo "Folder .oh-my-zsh already exists and it's being removed..."
     rm -rf ~/.oh-my-zsh
 fi
 
+echo "Cloning oh-my-zsh..."
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 
 if [ -f ~/.zshrc ]; then
     mv ~/.zshrc ~/.zshrc.bak
 fi
 
+echo "Setting zsh as default shell..."
 chsh -s `which zsh`
 
 if [ -d $DOTFILES ]; then
+    echo "Folder $DOTFILES already exists and it's being removed..."
     rm -rf $DOTILES
 fi
 
+echo "Cloning dotfiles..."
 git clone git://github.com/ondrowan/dotfiles.git $DOTFILES
 
+echo "Symlinking .zshrc..."
 ln $DOTFILES/.zshrc ~/.zshrc
