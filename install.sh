23c0774becaa7ecb634e751830a2371b17d39e99
ondrowan <ondrowan@gmail.com>
2013-01-04 16:55:40 +0100

    Added setuptools to install as a prerequisite to pip

--- install.sh
+++ install.sh
@@ -12,12 +12,12 @@ if [ ! -d $SITES ]; then
     mkdir $SITES
 fi
 
+# stuff
+sudo apt-get install git zsh ack curl mc python-setuptools apache2 mysql-server postgresql php5 php5-cli php5-curl php5-gd php5-imagick php5-intl php5-mongo php5-mysqlnd php5-pgsql php5-sqlite php5-suhosin php5-xdebug
+
 # pip & virtualenv
 sudo easy_install pip
 sudo pip install virtualenv virtualenvwrapper
-
-# stuff
-sudo apt-get install git zsh ack curl mc apache2 mysql-server postgresql php5 php5-cli php5-curl php5-gd php5-imagick php5-intl php5-mongo php5-mysqlnd php5-pgsql php5-sqlite php5-suhosin php5-xdebug
 
 # oh-my-zsh
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
