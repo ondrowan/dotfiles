72d6e6e14e0ade276f18bc21dfc9c2faed957e0f
ondrowan <ondrowan@gmail.com>
2013-01-04 21:28:15 +0100

    Fixed typo in colors. Changed color to blue.

--- install.sh
+++ install.sh
@@ -6,12 +6,12 @@ SITES=~/Sites
 DOTFILES=~/.dotfiles
 
 if [ ! -d $CODE ]; then
-    echo "\033[0;33mCreating $CODE directory...\e[00m"
+    echo "\033[0;34mCreating $CODE directory...\033[0m"
     mkdir $CODE
 fi
 
 if [ ! -d $SITES ]; then
-    echo "\033[0;33mCreating $SITES directory...\e[00m"
+    echo "\033[0;34mCreating $SITES directory...\033[0m"
     mkdir $SITES
 fi
 
@@ -21,7 +21,7 @@ sudo apt-get install git vim zsh pidgin ack curl mc python-setuptools apache2 my
 # pip & virtualenv
 
 if command -v pip > /dev/null 2>&1; then
-    echo "\033[0;33mInstalling pip...\e[00m"
+    echo "\033[0;34mInstalling pip...\033[0m"
     sudo easy_install pip
 fi
 
@@ -29,27 +29,27 @@ sudo pip install virtualenv virtualenvwrapper
 
 # oh-my-zsh
 if [ -d ~/.oh-my-zsh ]; then
-    echo "\033[0;33mFolder .oh-my-zsh already exists and it's being removed...\e[00m"
+    echo "\033[0;34mFolder .oh-my-zsh already exists and it's being removed...\033[0m"
     rm -rf ~/.oh-my-zsh
 fi
 
-echo "\033[0;33mCloning oh-my-zsh...\e[00m"
+echo "\033[0;34mCloning oh-my-zsh...\033[0m"
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 
 if [ -f ~/.zshrc ] || [ -h ~/.zshrc ]; then
     mv -b ~/.zshrc ~/.zshrc.bak
 fi
 
-echo "\033[0;33mSetting zsh as default shell...\e[00m"
+echo "\033[0;34mSetting zsh as default shell...\033[0m"
 chsh -s `which zsh`
 
 if [ -d $DOTFILES ]; then
-    echo "\033[0;33mFolder $DOTFILES already exists and it's being removed...\e[00m"
+    echo "\033[0;34mFolder $DOTFILES already exists and it's being removed...\033[0m"
     rm -rf $DOTFILES
 fi
 
-echo "\033[0;33mCloning dotfiles...\e[00m"
+echo "\033[0;34mCloning dotfiles...\033[0m"
 git clone git://github.com/ondrowan/dotfiles.git $DOTFILES
 
-echo "\033[0;33mSymlinking .zshrc...\e[00m"
+echo "\033[0;34mSymlinking .zshrc...\033[0m"
 ln $DOTFILES/.zshrc ~/.zshrc
